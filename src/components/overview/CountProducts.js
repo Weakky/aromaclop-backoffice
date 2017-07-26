f6b83dbfcc32f5d6a4e8ff3887cefcdbb4a41dcb
clement <clement.deseine@epitech.eu>
2017-07-26 13:08:06 +0200

    add color and migrate to react-chartjs-2

--- src/components/overview/CountProducts.js
+++ src/components/overview/CountProducts.js
@@ -1,45 +1,73 @@
 import React, { Component } from 'react';
 import { graphql } from 'react-apollo';
 import gql from 'graphql-tag';
-import { Bar } from 'react-chartjs';
+import { Bar } from 'react-chartjs-2';
+import './styles/CountProducts.css'
+
 
 class CountProducts extends Component {
     render() {
-        if (this.props.data.loading)
+        const {
+            loading,
+            fullyAvailableProductCount,
+            partiallyAvailableProductCount,
+            notAvailableProductCount
+        } = this.props.data;
+
+        if (loading)
             return(<h1>loading</h1>);
+
         const data = {
              labels: ["Disponibles", "Partiellement disponibles", "Indisponibles"],
              datasets: [{
                  label: 'Available product count',
                  data: [
-                     this.props.data.fullyAvailableProductCount.count,
-                     this.props.data.partiallyAvailableProductCount.count,
-                     this.props.data.notAvailableProductCount.count],
+                     fullyAvailableProductCount.count,
+                     partiallyAvailableProductCount.count,
+                     notAvailableProductCount.count],
+                 backgroundColor: "rgba(0,102,204,0.5)",
+                 borderColor: "rgba(0,91,183,0.5)",
+                 borderWidth: 1,
+                 hoverBackgroundColor: "rgba(0,102,204,0.8)",
+                 hoverBorderColor: "rgba(0,91,183,0.8)",
              }]};
 
-            return (
-                <div>
-                    <Bar data={data} width="600" height="250"/>
-                    <p>Total des produits: {this.props.data.allProductCount.count}</p>
-                </div>);
+        const options = {
+            scales: {
+                yAxes: [{
+                    ticks: {
+                        beginAtZero: true
+                    }
+                }]
+            }
+        };
+
+        return (
+            <div>
+                <div className="Countproduct-chart">
+                    <Bar data={data} options={options}/>
+                </div>
+                <p className="Countproduct-summary">
+                    Total des produits: {this.props.data.allProductCount.count}
+                </p>
+            </div>);
         }
 }
 
 const CountProductsQuery = gql`query allProductsCount {
     allProductCount: _allProductsMeta { count }
     fullyAvailableProductCount: _allProductsMeta(filter: {
-      productTaxons_every: { available: true }})
-      { count }
+        productTaxons_every: { available: true }})
+        { count }
   	partiallyAvailableProductCount: _allProductsMeta(filter: {
-      AND: [
-        {productTaxons_some: { available: true }},
-        {productTaxons_some: { available: false }} 
-      ]
-    })
-	 { count }
-  notAvailableProductCount: _allProductsMeta(filter: {
-  	productTaxons_every: { available: false }})
-    { count }
-}`;
+        AND: [
+            {productTaxons_some: { available: true }},
+            {productTaxons_some: { available: false }} 
+        ]})
+        { count }
+    notAvailableProductCount: _allProductsMeta(filter: {
+        productTaxons_every: { available: false }})
+        { count }
+    }`;
 
 export default graphql(CountProductsQuery)(CountProducts);
