38997859c359afbe6e65d33d27c022210b69f086
Jules <jules.dupont@epitech.eu>
2017-10-10 20:38:13 +0200

    Feature status indicator

--- src/pages/layout/components/Sidebar.js
+++ src/pages/layout/components/Sidebar.js
@@ -10,6 +10,15 @@ const Sidebar = props => (
       <span className="Sidebar-title">Aromaclop</span>
     </div>
     <div className="Sidebar-nav" {...props} />
+    <div className="Sidebar-status">
+      <a target="_blank" href="https://status.graph.cool/">
+        <img
+          src={require("../../../assets/img/graphcool.png")}
+          className="Sidebar-status-img"
+        />
+      </a>
+      <span className="Sidebar-status-label">Status des services</span>
+    </div>
   </div>
 );
 
